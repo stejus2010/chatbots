11cdda990353e7533b9dd5fb384d0d52a11c8dc7
stejus2010 <115551989+stejus2010@users.noreply.github.com>
2023-10-31 19:48:20 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = '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'; // Paste your API key here
+const API_KEY = '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'; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 
